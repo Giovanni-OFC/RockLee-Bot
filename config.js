dd3cd65857ebaaa8368e3c8ecdeca6b4a8581434
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-21 21:35:36 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.owner = [
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['51902855352', '51902855352']
+global.prems = ['56986511915', '56986511915']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
